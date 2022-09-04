910e9361aaba239d498bcb30c09db63360e2b64a
hieubkvn123 <javapythonscala@gmail.com>
2022-09-05 01:20:08 +0800

    fix: Minor fix to download.sh

--- competitions/web-traffic-forecasting/download.sh
+++ competitions/web-traffic-forecasting/download.sh
@@ -6,3 +6,13 @@ mkdir data
 # 2. Download
 cd data
 kaggle competitions download -c web-traffic-time-series-forecasting
+
+# 3. Unzip all files
+unzip web-traffic-time-series-forecasting.zip
+unzip train_1.csv.zip
+unzip train_2.csv.zip
+unzip key_1.csv.zip
+unzip key_2.csv.zip
+unzip sample_submission_1.csv.zip
+unzip sample_submission_2.csv.zip
+rm *.zip
